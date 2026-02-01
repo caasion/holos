0ccd22d6c383c3b764d351d5e44d0bf869e74dc1
Isaac (Tsz Lai) Ng <103465188+caasion@users.noreply.github.com>
2026-02-01 17:25:52 -0500

    Refactor templateActions,Store, and trackActions

--- src/tracks/trackActions.ts
+++ src/tracks/trackActions.ts
@@ -1,151 +1,78 @@
-import type { ISODate, DataService, HelperService, ItemMeta, CalendarMeta, ItemID, PluginSettings } from '../plugin/types';
+import type { ISODate, DataService, HelperService, CalendarMeta, PluginSettings, TDate, Track } from '../plugin/types';
 import { get } from 'svelte/store';
 import { CalendarPipeline } from '../calendar/calendarPipelines';
 import { addDays, parseISO, startOfDay } from 'date-fns';
 import { Menu, Notice, type App } from 'obsidian';
 import { NewItemModal } from 'src/templates/NewItemModal';
 import { GenericEditModal } from 'src/templates/EditItemModal';
-import { getTemplate, sortedTemplateDates } from 'src/templates/templatesStore';
-import { NewTemplateModal } from 'src/templates/NewTemplateModal';
 import { ConfirmationModal } from 'src/plugin/ConfirmationModal';
+import type { TemplateActions } from 'src/templates/templateActions';
 
-export interface PlannerServiceDeps {
+export interface TrackActionsDeps {
     settings: PluginSettings;
+    templateActions: TemplateActions;
     data: DataService;
     helpers: HelperService;
     calendarPipelines: CalendarPipeline;
 }
 
-export class PlannerActions {
+export class TrackActions {
     private settings: PluginSettings;
+    private templates: TemplateActions;
     private data: DataService;
-    private helpers: HelperService;
     private calendarPipelines: CalendarPipeline;
 
-    constructor(deps: PlannerServiceDeps) {
+    constructor(deps: TrackActionsDeps) {
         this.settings = deps.settings;
+        this.templates = deps.templateActions;
         this.data = deps.data;
-        this.helpers = deps.helpers;
         this.calendarPipelines = deps.calendarPipelines;
     }
 
-    /** Uses binary search to get the template date for a given date. Returns blank if not found. */
-    public getTemplateDate(date: ISODate): ISODate {
-        const dates: ISODate[] = get(sortedTemplateDates);
+    // ===== Reading trakcs ===== /
+    
+    /** Gets the metadata of an item given a date with a template */
+    public getTrack(tDate: TDate, id: string): Track {
+        return this.templates.getTemplate(tDate)[id];
+    }
 
-		// Implement binary search to find the template date that is the greatest date less than or equal to the date provided
-		let left = 0;
-		let right = dates.length - 1;
-		let result: ISODate = "";
-
-		while (left <= right) {
-			const mid = Math.floor((left + right) / 2);
-			const midDate = dates[mid];
-
-			if (midDate === date) {
-				return midDate;
-			}
-			if (midDate < date) {
-				result = midDate;
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-
-		return result;
-	}
-
-    /** Creates a new item given a date and item details. */
-    public newItem(date: ISODate, meta: ItemMeta) {
-        this.data.updateItemMeta(date, meta.id, meta);
-
-        const templateDate = this.getTemplateDate(date); 
-        this.data.templates.update(templates => {
-            const currentMetaRecord = templates[templateDate] || {};
-            const newMetaRecord = { ...currentMetaRecord, [meta.id]: {...meta, order: Object.keys(currentMetaRecord).length } };
-            return { ...templates, [templateDate]: newMetaRecord };
-        });
-
-        if (meta.type === "calendar") {
-            const from = parseISO(date);
-            const to = addDays(from, this.settings.lookaheadDays)
-
-            this.calendarPipelines.fetchInGracePeriod(
-                templateDate,
-                meta.id, 
-                from, 
-                addDays(to, this.settings.lookaheadDays)
-            );
-
-            new Notice(`Fetched Calendar "${meta.label}" from ${date} to ${this.helpers.getISODate(to)}.`);
+    /** Returns the id of a track from a specified template given the item label (case insensitive). */
+    public getTrackIDFromLabel(tDate: TDate, label: string): string {
+        const template = this.templates.getTemplate(tDate);
+        
+        // Guard: if no template exists for this date, return empty string
+        if (!template) {
+            return "";
         }
+    
+        for (const item of Object.values(template)) {
+            if (label.toLowerCase() == item.label.toLowerCase()) {
+                return item.id;
+            }
+        }
+    
+        return "";
     }
 
-    /** Handles the deletion of an item given the template date and id (confirmation modal, deletion, and clean-up). */
-    public handleRemoveItem(app: App, tDate: ISODate, id: ItemID) {
-        new ConfirmationModal(
-            app, 
-            () => this.removeItem(tDate, id),
-            "Remove",
-            "Removing the item will remove all cell contents."
-        ).open();
-    }
+    // ===== Creating tracks ===== // 
+    
+    // ===== Modifying tracks ===== //
 
-    /** Deletes an item and its cell contents */
-    public removeItem(tDate: ISODate, id: ItemID): boolean {
-        if (!get(this.data.templates)[tDate]) return false;
+    /** Updates the metadata of an item given a date with a template, the item's id, and a partial object containing the updates. Returns false if given date doesn't have a template. */
+    public updateTrack(tDate: TDate, id: string, updates: Partial<Track>): boolean {
+        const currTemplate = this.templates.getTemplate(tDate);
+        if (!currTemplate) return false;
 
-        this.data.removeFromTemplate(tDate, id);
-        this.data.removeFromCellsInTemplate(tDate, id);
-
+        this.templates.setTemplate(tDate, {
+            ...currTemplate, 
+            [id]: { ...currTemplate[id], ...updates } as Track
+        })
+    
         return true;
     }
 
-    /** Handles the create of a new template (modal and creation). */
-    public handleNewTemplate(app: App) {
-        new NewTemplateModal(app, this.helpers.getISODate(new Date()), (date, copyFrom) => this.data.setTemplate(date, copyFrom == '' ? {} : this.data.getTemplate(copyFrom))).open();
-    }
-
-    /** Handles the removal of a new template (confirmation modal, deletion, and clean-up.) */
-    public handleRemoveTemplate(app: App, tDate: ISODate) {
-        new ConfirmationModal(
-            app, 
-            () => this.data.removeTemplate(tDate), 
-            "Remove",
-            "Removing the template will remove all items and their contents."
-        ).open();
-    }
-
-    /** Creates and opens the context menu for creating a new item. */
-    public newItemMenu(app: App, evt: MouseEvent, tDate: ISODate): void {
-        evt.preventDefault();
-        evt.stopPropagation();
-    
-        const menu = new Menu();
-
-        menu
-        .addItem((i) =>
-            i.setTitle("Create New Action Item")
-            .setIcon("add")
-            .onClick(() => {
-                new NewItemModal(app, "action", tDate, (date: ISODate, meta: ItemMeta) => this.newItem(date, meta)).open();
-            })
-        )
-        .addItem((i) =>
-            i.setTitle("Add New Remote Calendar")
-            .setIcon("add")
-            .onClick(() => {
-                new NewItemModal(app, "calendar", tDate, (date: ISODate, meta: ItemMeta) => this.newItem(date, meta as CalendarMeta)).open();
-            })
-        )
-        
-    
-        menu.showAtPosition({ x: evt.clientX, y: evt.clientY });
-    }
-
     /** Creates and opens the context menu for an item. */
-    public openItemMenu(app: App, evt: MouseEvent, date: ISODate, id: ItemID, meta: ItemMeta) {
+    public openTrackMenu(app: App, evt: MouseEvent, date: ISODate, id: string, meta: Track) {
         evt.preventDefault();
         evt.stopPropagation();
 
@@ -161,49 +88,77 @@ export class PlannerActions {
                 i.setTitle("Edit")
                 .setIcon("pencil")
                 .onClick(() => {
-                    new GenericEditModal(app, meta, (newMeta) => this.data.updateItemMeta(this.getTemplateDate(date), id, newMeta)).open();
+                    new GenericEditModal(app, meta, (newMeta) => this.data.updateItemMeta(this.templates.getTemplateDate(date), id, newMeta)).open();
                 })
             )
             .addItem((i) =>
                 i.setTitle("Remove")
                 .setIcon("x")
                 .onClick(() => {
-                    this.handleRemoveItem(app, this.getTemplateDate(date), id);
+                    this.handleRemoveTrack(app, this.templates.getTemplateDate(date), id);
                 })
             )
-
-        if (meta.type === "calendar") {
-            const from = parseISO(date);
-            const to = addDays(from, this.settings.lookaheadDays)
-
-            menu.addItem((i) => 
-                i.setTitle(`Fetch Calendar (from ${date} to ${this.helpers.getISODate(to)})`)
-                .setIcon("pencil")
-                .onClick(() => {
-                    this.calendarPipelines.fetchInGracePeriod(
-                        this.getTemplateDate(date),
-                        meta.id, 
-                        from, 
-                        to
-                    );
-                })
-            )
-        }
 
         menu.showAtPosition({ x: evt.clientX, y: evt.clientY });
     }
 
-    /** Swaps two items within a template given the id of the first item, and the distance to swap the item to. A costly operation. */
-    public swapItem(tDate: ISODate, id: ItemID, dist: number): boolean {
-        const currOrder = this.data.getItemMeta(tDate, id).order;
+    /** Swaps two tracks within a template given the id of the first track, and the distance to swap the track to. A costly operation. */
+    public swapTracks(tDate: ISODate, id: string, dist: number): boolean {
+        const currOrder = this.getTrack(tDate, id).order;
         const newOrder = currOrder + dist;
         const swapTargetID = Object.values(this.data.getTemplate(tDate)).find(t => t.order == newOrder)?.id;
 
         if (!swapTargetID) return false;
 
-        this.data.updateItemMeta(tDate, id, {order: newOrder});
-        this.data.updateItemMeta(tDate, swapTargetID, {order: currOrder});
+        // WILL FIX LATER
+
+        this.updateTrack(tDate, id, {order: newOrder});
+        this.updateTrack(tDate, swapTargetID, {order: currOrder});
 
         return true;
+    }
+
+    // ===== Deleting tracks ===== //
+
+    /** Handles the deletion of a track given the template date and id (confirmation modal, deletion, and clean-up). */
+    public handleRemoveTrack(app: App, tDate: ISODate, id: string) {
+        new ConfirmationModal(
+            app, 
+            () => this.removeTrack(tDate, id),
+            "Remove",
+            "Removing the track will not remove all cell contents."
+        ).open();
+    }
+
+    /** Deletes an track and its cell contents */
+    public removeTrack(tDate: ISODate, id: string): boolean {
+        if (!this.templates.getTemplate(tDate)) return false;
+
+        // WILL FIX LATER
+
+        this.data.removeFromTemplate(tDate, id);
+        this.data.removeFromCellsInTemplate(tDate, id);
+
+        return true;
+    }
+
+    // ===== Reading data from tracks ===== //
+    
+    /** Sets the contents of a floatCell for a given track id.
+     * Doesn't matter if the cell was previously empty.
+     */
+    public setFloatCell(tDate: TDate, id: string, value: Element[]): boolean {    
+        // NOT IMPLEMENTED
+    
+        return false;
+    }
+    
+    
+    /** Gets the contents of a float cell given an track id. 
+     * Returns an empty string if there is no template, track, or floatCell value found. */
+    public getFloatCell(tDate: TDate, id: string) {
+        // NOT IMPLEMENTED
+    
+        return "";
     }
 }
